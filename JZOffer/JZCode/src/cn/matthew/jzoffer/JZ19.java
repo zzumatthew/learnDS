2f74377e2331b857eb90346f510d17d08b90e908
zzumatthew <zzumatthew@163.com>
2020-08-10 16:26:46 +0800

    笔记修改

---
+++ JZOffer/JZCode/src/cn/matthew/jzoffer/JZ19.java
@@ -0,0 +1,22 @@
+package cn.matthew.jzoffer;
+
+/**
+ * @ClassName JZ19
+ * @Description 正则表达式匹配
+ * @Author iematthew
+ * @Date 2020/8/10 10:31
+ * @Version 1.0
+ **/
+
+/*
+题目：
+请实现一个函数来匹配包含'.'和'*'的正则表达式。
+模式中的字符.代表任意字符，*代表它前面的字符可以出现任意多次（含0）。
+在本题中，匹配是指字符串的所有字符匹配整个模式。
+例如，字符串"aaa"与模式"a.a"和"ab*ac*a"匹配，但是与"aa.a"和"ab*a"均不匹配。
+
+
+ */
+public class JZ19 {
+
+}
---
+++ JZOffer/JZCode/src/cn/matthew/jzoffer/JZ21.java
@@ -0,0 +1,13 @@
+package cn.matthew.jzoffer;/**
+  *@ClassName JZ21
+  *@Description TODO
+  *@Author iematthew
+  *@Date 2020/8/10 16:19
+  *@Version 1.0
+    **/
+    
+/*
+ 
+ */
+public class JZ21 {
+}
