b987467a7c1cab10443dbb3aa35c4553cbfa8957
zzumatthew <zzumatthew@gmail.com>
2020-08-15 09:57:26 +0800

    JZ36 二叉搜索树与双向链表

---
+++ JZOffer/JZCode/src/cn/matthew/jzoffer/JZ37.java
@@ -0,0 +1,16 @@
+package cn.matthew.jzoffer;
+
+/**
+ * @ClassName JZ37
+ * @Description 序列化二叉树
+ * @Author iematthew
+ * @Date 2020/8/15 09:34
+ * @Version 1.0
+ **/
+
+/*
+题目：
+请实现两个函数，分别用来序列化和反序列化二叉树。
+ */
+public class JZ37 {
+}
