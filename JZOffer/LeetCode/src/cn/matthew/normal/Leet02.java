6f730218b69d30d4d3e5f948237c8d2fefdd713e
zzumatthew <zzumatthew@gmail.com>
2020-08-15 14:58:56 +0800

    Leetcode02 两数相加

---
+++ JZOffer/LeetCode/src/cn/matthew/normal/Leet02.java
@@ -0,0 +1,33 @@
+package cn.matthew.normal;
+
+/**
+ * @ClassName Leet02
+ * @Description LC02 两数相加
+ * @Author iematthew
+ * @Date 2020/8/15 12:05
+ * @Version 1.0
+ **/
+
+/*
+题目：
+给出两个非空的链表用来表示两个非负的整数。其中，它们各自的位数是按照逆序的方式存储的，
+并且它们的每个节点只能存储一位数字。
+如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。
+您可以假设除了数字0之外这两个数都不会以0开头。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/add-two-numbers
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+思路：
+1.先把两个链表反转得到两个数，
+2.然后两个数相加
+3.和数
+ */
+public class Leet02 {
+    public static void main(String[] args) {
+
+    }
+
+
+}
