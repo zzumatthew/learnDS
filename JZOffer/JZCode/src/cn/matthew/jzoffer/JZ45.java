80bcbaeefb32f5c90961e79656ccbec16ca9bfd8
zzumatthew <zzumatthew@gmail.com>
2020-08-18 18:24:15 +0800

    JZ45 把数组排成最小的数

---
+++ JZOffer/JZCode/src/cn/matthew/jzoffer/JZ45.java
@@ -0,0 +1,25 @@
+package cn.matthew.jzoffer;
+
+/**
+ * @ClassName JZ45
+ * @Description 把数组排成最小的数
+ * @Author iematthew
+ * @Date 2020/8/17 17:38
+ * @Version 1.0
+ **/
+
+/*
+题目：
+输入一个正整数数组，把数组里所有数字拼接起来排成一个数、
+打印能拼接出的所有数字中最小的一个。
+例如，输入数组{3,32,321}，则打印出这3个数字能排成的最小数字321323。
+
+思路：
+这个问题最原始的解法就是全排列。
+
+ */
+public class JZ45 {
+    public static void main(String[] args) {
+
+    }
+}
