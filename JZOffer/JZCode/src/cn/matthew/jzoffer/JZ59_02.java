04f0cebae8aa1b8dc213a348ca772c140b6ae3c6
zzumatthew <zzumatthew@gmail.com>
2020-09-02 11:21:41 +0800

    JZ59_02 队列的最大值（看不懂题目，跳过）

---
+++ JZOffer/JZCode/src/cn/matthew/jzoffer/JZ59_02.java
@@ -0,0 +1,21 @@
+package cn.matthew.jzoffer;
+
+/**
+ * @ClassName JZ59_02
+ * @Description 队列的最大值
+ * @Author iematthew
+ * @Date 2020/9/02 10:45
+ * @Version 1.0
+ **/
+
+/*
+题目：
+请定义一个队列并实现函数max_value得到队列里的最大值
+要求函数max_value,push_back,pop_front的均摊时间复杂度为O(1)
+若队列为空，pop_front和max_value需要返回-1。
+
+这个题目在说什么？！
+看不懂，放弃。
+ */
+public class JZ59_02 {
+}
