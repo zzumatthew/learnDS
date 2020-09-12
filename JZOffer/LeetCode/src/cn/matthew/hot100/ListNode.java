f4d5d54f316f5a1aa33bb4a0f45ac9ea75491616
zzumatthew <zzumatthew@gmail.com>
2020-09-12 22:23:27 +0800

    ListNode类

---
+++ JZOffer/LeetCode/src/cn/matthew/hot100/ListNode.java
@@ -0,0 +1,21 @@
+package cn.matthew.hot100;
+
+/**
+ * @ClassName ListNode
+ * @Description TODO
+ * @Author iematthew
+ * @Date 2020/9/12 22:17
+ * @Version 1.0
+ **/
+
+/*
+
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
